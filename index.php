d32f64a8e66ebb5a90228c047106d7045c55bc29
felipeoli7eira <me.felipeoliveira@gmail.com>
2020-10-28 02:16:46 -0300

    base

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Felipe Oliveira | FullStack PHP Developer</title>
+</head>
+<body>
+    <main class="app">
+        <p>em breve...</p>
+    </main>
+</body>
+</html>
